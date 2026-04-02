d48ec880711759738d7f3b54e5e0e92a23777e71
sagarprojects02 <saisagaryadav143@gmail.com>
2026-04-02 23:14:20 +0530

    Create metaclasses.py

---
+++ src/my_advanced_lib/core/metaclasses.py
@@ -0,0 +1,7 @@
+class Meta(type):
+    registry = {}
+
+    def __new__(cls, name, bases, dct):
+        new_class = super().__new__(cls, name, bases, dct)
+        cls.registry[name] = new_class
+        return new_class
